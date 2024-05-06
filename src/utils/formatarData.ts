a8f3bb21387c402bfddbbb89b4f020f0f9668b57
Lílian Geovana <119058582+liliangeovana@users.noreply.github.com>
2024-05-06 16:20:36 -0300

    Controle estoque escola (#5)
    * ajustes no login, cadastro de estoque, tela de controle de estque v1.0
    
    * estoque escola v1.0

---
+++ src/utils/formatarData.ts
@@ -0,0 +1,9 @@
+// utils.js
+export const formatarData = (data: any) => {
+    const dataObj = new Date(data);
+    const dia = dataObj.getDate().toString().padStart(2, '0');
+    const mes = (dataObj.getMonth() + 1).toString().padStart(2, '0');
+    const ano = dataObj.getFullYear();
+    return `${dia}/${mes}/${ano}`;
+  };
+  
