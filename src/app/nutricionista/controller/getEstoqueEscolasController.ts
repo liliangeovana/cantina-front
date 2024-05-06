b90d3d777f4b6af22ab914fd91f827943e4a509d
Lílian Geovana <119058582+liliangeovana@users.noreply.github.com>
2024-05-06 19:01:39 -0300

    tela de nutricionistas-estoques por escola (#6)

---
+++ src/app/nutricionista/controller/getEstoqueEscolasController.ts
@@ -0,0 +1,27 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+const useBuscarEstoquesController = () => {
+  const [estoquesEscolas, setEstoques] = useState<any[]>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+
+  const fetchEstoques = async () => {
+    try {
+      const response = await axios.get("/api/escolas/estoques/");
+      const data = response.data.data;
+      setEstoques(data);
+    } catch (error) {
+      console.error("Erro ao buscar estoques:", error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchEstoques();
+  }, []);
+
+  return { estoquesEscolas, isLoading };
+};
+
+export default useBuscarEstoquesController;
