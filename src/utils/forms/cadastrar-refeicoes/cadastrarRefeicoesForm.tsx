76189adce7dccb6baa340762d77e03a328d9e6ac
Wandeilson Fernandes <wandeilsonfernandes2019@outlook.com>
2024-05-06 20:54:00 -0400

    Adicionando campos
    campos de input, total de alunos, nome da refeicão, ingredietes e quantidades

--- src/utils/forms/cadastrar-refeicoes/cadastrarRefeicoesForm.tsx
+++ src/utils/forms/cadastrar-refeicoes/cadastrarRefeicoesForm.tsx
@@ -1,70 +1,76 @@
 'use client'
-import useCadastrarRefeicoesController from "@/utils/forms/cadastrar-refeicoes/cadastrarRefeicoesForm";
 
 export default function cadastrarRefeicoesForm() {
-
-    // Função para gerar um número específico de campos do formulário
-    const renderizarCampos = (quantidade:any) => {
-        const campos = [];
-        for (let i = 0; i < quantidade; i++) {
-            campos.push(
-                <div key={i} className="flex flex-row gap-8 h-fit w-full px-16 py-4">
-                   
-                    {/* Tipo */}
-                    <div className="flex flex-col gap-1 w-30">
-                        <label htmlFor={`quantidade-${i}`}>Nome</label>
-                        <input
+    return (
+        <div>
+          <link rel="stylesheet" href="https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css" />
+          <div className="flex flex-row">
+          </div>
+                      <div className="text-center p-8 uppercase text-cor3 font-semibold">
+                          <h2>Cadastro de Refeição</h2>
+                      </div>
+                      
+                      <div className="grid grid-cols-5 text-center gap-4 p-2 overflow-auto">
+                        <div className="flex flex-col gap-5">
+                          <h3 className="font-semibold">Refeição</h3>
+                          <div className="flex flex-col gap-3 m-auto">
+                
+                                <div  className="w-44">
+                                <input
                             className="p-2 border border-gray-400 rounded-lg focus:outline-none focus:border-gray-600"
                             placeholder="INSIRA O NOME"
                         />
-                    </div>
-                     {/* Quantidade */}
-                     <div className="flex flex-col gap-1 w-24">
-                        <label htmlFor={`quantidade-${i}`}>Alunos</label>
-                        <input
-                            className="p-2 border border-gray-400 rounded-lg focus:outline-none focus:border-gray-600"
+                        </div>
+                          </div>
+                        </div>
+                        <div className="flex flex-col gap-5">
+                          <h3 className="font-semibold">Total de Alunos</h3>
+                          <div className="flex flex-col gap-3 m-auto">
+                          <input
+                            className="p-2 border border-gray-400 rounded-lg focus:outline-none focus:border-gray-600 text-black"
                             placeholder="1"
                         />
-                    </div>
-                                       {/* Classificacao */}
-                                       <div className="flex flex-col gap-1">
-                        <label htmlFor={`classificacao-${i}`}>Ingredientes</label>
-                        <select
+                          </div>
+                        </div>
+                        <div className="flex flex-col gap-5">
+                          <h3 className="font-semibold">Ingredientes</h3>
+                          <div className="flex flex-col gap-3 m-auto">
+
+                          <select
                             className="p-2 border border-gray-400 rounded-lg focus:outline-none focus:border-gray-600 text-black"
-                            id={`classificacao-${i}`}
-                            name={`classificacao-${i}`}
                         >
                             <option selected hidden>CEBOLA</option>
                             <option value="embalagem">CEBOLA</option>
                             <option value="kg">PIMENTA</option>
                             <option value="frasco">ARROZ</option>
                         </select>
-                    </div>
-                  
-                     {/* Quantidade */}
-                     <div className="flex flex-col gap-1 w-24">
-                        <label htmlFor={`quantidade-${i}`}>Quantidade</label>
-                        <input
-                            className="p-2 border border-gray-400 rounded-lg focus:outline-none focus:border-gray-600"
-                            id={`quantidade-${i}`}
-                            type="number"
-                            name={`quantidade-${i}`}
-                            placeholder="3g"
+                        </div>
+                        </div>
+                        <div className="flex flex-col gap-5">
+                          <h3 className="font-semibold">Quantidade</h3>
+                          <div className="flex flex-col gap-3 m-auto">
+                          <input
+                            className="p-2 border border-gray-400 rounded-lg focus:outline-none focus:border-gray-600 text-black"
+                            placeholder="3 GRAMAS"
                         />
+                          </div>
+
+                        </div>
+
                     </div>
-
- 
+                    <div className="text-left ml-11 mt-4 mx-auto p-8 uppercase text-black font-semibold">
+                          <h2>Descrição de preparo</h2>
+                         <label >
+                            <textarea rows={20} cols={50}/>
+                        </label>  
+                        </div> 
+                        <div className="text-center ml-20 mt-4 mx-auto p-8 uppercase text-black font-semibold">
+                        <button
+                    className={"w-80 p-2 mt-8 border bg-cor4 border-gray-300 hover:bg-green-600 text-white rounded-lg focus:outline-none focus:border-gray-600 "}>Enviar
+                </button>
                 </div>
-            );
-        }
-        return campos;
-    };
 
-    return (
-        <div className="flex flex-col items-center h-svh py-10">
-            <h1 className="text-cor3 font-medium uppercase"></h1>
-            
-            {renderizarCampos(4)} {/* Chama a função para renderizar 5 conjuntos de campos */}
         </div>
-    );
-}
+      );
+    };
+    
