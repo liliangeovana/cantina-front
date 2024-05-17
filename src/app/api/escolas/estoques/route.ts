2689994a49a5f041b4fe1740f4af0321898ec88c
Lílian <lili.ggsb@gmail.com>
2024-05-16 22:52:09 -0300

    teste remover cache

--- src/app/api/escolas/estoques/route.ts
+++ src/app/api/escolas/estoques/route.ts
@@ -6,6 +6,7 @@ import { connect } from "../../../../../db";
 export async function GET() {
     try {
         await connect();
+
         // Buscando todas as escolas
         const schools = await schoolModel.find({}, '_id nome').sort({ nome: 1 });
 
@@ -17,6 +18,9 @@ export async function GET() {
             // Buscando os ingredientes associados à escola
             const ingredientes = await Ingrediente.find({ school: school._id });
 
+            // Log de debug para verificar os ingredientes recuperados
+            console.log(`Escola: ${school.nome}, Ingredientes: ${JSON.stringify(ingredientes)}`);
+
             // Adicionando as informações da escola e ingredientes ao array
             schoolsWithIngredients.push({
                 school: {
@@ -27,9 +31,16 @@ export async function GET() {
             });
         }
 
-        return NextResponse.json({
+        return new NextResponse(JSON.stringify({
             message: "Escolas e ingredientes recuperados com sucesso",
             data: schoolsWithIngredients
+        }), {
+            headers: {
+                'Cache-Control': 'no-store, no-cache, must-revalidate, proxy-revalidate',
+                'Pragma': 'no-cache',
+                'Expires': '0',
+                'Surrogate-Control': 'no-store'
+            }
         });
     } catch (error) {
         console.error("Erro ao recuperar escolas e ingredientes:", error);
