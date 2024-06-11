37d5adc5b30ddd46641b78b59db4e5af3cedf756
Lílian <lili.ggsb@gmail.com>
2024-06-11 15:19:11 -0300

    ajuste home nutri e historico

--- src/app/nutricionista/historico-refeicoes/page.tsx
+++ src/app/nutricionista/historico-refeicoes/page.tsx
@@ -10,6 +10,7 @@ const HistoricoPage: React.FC = () => {
     const [escolaSelecionada, setEscolaSelecionada] = useState<string | undefined>(undefined);
     const [selectedMonth, setSelectedMonth] = useState<string>("");
     const [organizedMeals, setOrganizedMeals] = useState<{ [key: string]: { _id: string, date: string, nome: string, padraoMantido: boolean }[] }>({ "Manhã": [], "Tarde": [] });
+    const [refeicoesDaEscolaSelecionada, setRefeicoesDaEscolaSelecionada] = useState<any[]>([]);
 
     useEffect(() => {
         if (!isLoading && refeicoesEscolas.length > 0 && !escolaSelecionada) {
@@ -18,13 +19,21 @@ const HistoricoPage: React.FC = () => {
     }, [isLoading, refeicoesEscolas, escolaSelecionada]);
 
     useEffect(() => {
+        // Filtrando as refeições da escola selecionada
+        const filteredRefeicoes = refeicoesEscolas.filter(refeicaoEscola => refeicaoEscola.school._id === escolaSelecionada);
+        console.log('Refeições filtradas:', filteredRefeicoes);
+        const refeicoesDaEscola = filteredRefeicoes.length > 0 ? filteredRefeicoes[0].meals : [];
+        setRefeicoesDaEscolaSelecionada(refeicoesDaEscola);
+    }, [refeicoesEscolas, escolaSelecionada]);
+
+    useEffect(() => {
         const mealsByShift: { [key: string]: { _id: string, date: string, nome: string, padraoMantido: boolean }[] } = { "Manhã": [], "Tarde": [] };
-        refeicao.forEach(meal => {
+        refeicoesDaEscolaSelecionada.forEach(meal => {
             const date = new Date(meal.createdAt).toLocaleDateString();
             mealsByShift[meal.turno].push({ _id: meal._id, date, nome: meal.nome, padraoMantido: meal.padraoMantido });
         });
         setOrganizedMeals(mealsByShift);
-    }, [isLoading, refeicao]);
+    }, [refeicoesDaEscolaSelecionada]);
 
     useEffect(() => {
         const filteredMeals = refeicao.filter(meal => {
@@ -49,14 +58,12 @@ const HistoricoPage: React.FC = () => {
         setSelectedMonth(event.target.value);
     };
 
-    const filteredRefeicoes = refeicoesEscolas.filter(refeicaoEscola => refeicaoEscola.school._id === escolaSelecionada);
-
-    const uniqueMonths = Array.from(new Set(refeicao.map(meal => {
+    const uniqueMonths = Array.from(new Set(refeicoesDaEscolaSelecionada.map(meal => {
         const date = new Date(meal.createdAt);
         return `${date.getMonth() + 1}-${date.getFullYear()}`;
     })));
 
-    return (
+    return(
         <div className="flex flex-row h-svh">
             <div className='sticky top-0'>
                 <title>Histórico de refeições</title>
@@ -91,48 +98,49 @@ const HistoricoPage: React.FC = () => {
                 </div>
 
                 {isLoading ? (
-                    <div className="h-full flex justify-center items-center">
-                        <Loading />
-                    </div>
-                ) : (
-                    <div className="flex-1 p-10 overflow-auto">
-                        {/**TELA PRINCIPAL */}
-                        {filteredRefeicoes.length === 0 || filteredRefeicoes[0].meals.length === 0 ? (
-                            <div className="p-8 flex justify-center items-center">
-                                <p>Nenhuma refeição produzida</p>
-                            </div>
-                        ) : (
-                            <div className='flex flex-col'>
-                                <p className='text-red-500 text-xs pb-3 text-right'>*selecione uma refeição para mais detalhes</p>
-                                <div className='grid grid-cols-2 gap-10 overflow-auto'>
-                                    {["Manhã", "Tarde"].map(shift => (
-                                        <div key={shift}>
-                                            <h2 className='font-semibold text-center mb-6 text-cor3 uppercase'>{shift}</h2>
-                                            <div>
-                                                {organizedMeals[shift] && organizedMeals[shift].length > 0 ? (
-                                                    organizedMeals[shift].map((meal, index) => {
-                                                        return (
-                                                            <div key={index} className="flex flex-col gap-2 mb-6 text-center p-4">
-                                                                <span className='font-semibold text-sm '>{meal.date}</span>
-                                                                <Link className={meal.padraoMantido ? 'bg-white border rounded-md p-2 shadow-sm cursor-pointer hover:shadow-md' : 'bg-white border rounded-md border-red-500 p-2 shadow-sm cursor-pointer hover:shadow-md'} href={`/nutricionista/historico-refeicoes/detalhes/${meal._id}`}>
-                                                                    <span>
-                                                                        {meal.nome}
-                                                                    </span>
-                                                                </Link>
-                                                            </div>
-                                                        );
-                                                    })
-                                                ) : (
-                                                    <div className='text-center'>Nenhuma refeição cadastrada</div>
-                                                )}
+    <div className="h-full flex justify-center items-center">
+        <Loading />
+    </div>
+) : (
+    <div className="flex-1 p-10 overflow-auto">
+        {/**TELA PRINCIPAL */}
+        {refeicoesDaEscolaSelecionada.length === 0 ? (
+            <div className="p-8 flex justify-center items-center">
+                <p>Nenhuma refeição produzida</p>
+            </div>
+        ) : (
+            <div className='flex flex-col'>
+                <p className='text-red-500 text-xs pb-3 text-right'>*selecione uma refeição para mais detalhes</p>
+                <div className='grid grid-cols-2 gap-10 overflow-auto'>
+                    {["Manhã", "Tarde"].map(shift => (
+                        <div key={shift}>
+                            <h2 className='font-semibold text-center mb-6 text-cor3 uppercase'>{shift}</h2>
+                            <div>
+                                {organizedMeals[shift] && organizedMeals[shift].length > 0 ? (
+                                    organizedMeals[shift].map((meal, index) => {
+                                        return (
+                                            <div key={index} className="flex flex-col gap-2 mb-6 text-center p-4">
+                                                <span className='font-semibold text-sm '>{meal.date}</span>
+                                                <Link className={meal.padraoMantido ? 'bg-white border rounded-md p-2 shadow-sm cursor-pointer hover:shadow-md' : 'bg-white border rounded-md border-red-500 p-2 shadow-sm cursor-pointer hover:shadow-md'} href={`/nutricionista/historico-refeicoes/detalhes/${meal._id}`}>
+                                                    <span>
+                                                        {meal.nome}
+                                                    </span>
+                                                </Link>
                                             </div>
-                                        </div>
-                                    ))}
-                                </div>
+                                        );
+                                    })
+                                ) : (
+                                    <div className='text-center'>Nenhuma refeição cadastrada</div>
+                                )}
                             </div>
-                        )}
-                    </div>
-                )}
+                        </div>
+                    ))}
+                </div>
+            </div>
+        )}
+    </div>
+)}
+
             </div>
         </div>
     );
